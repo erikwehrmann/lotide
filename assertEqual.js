cc0729ab59ff143a56cf9c1d5a6b62ef9a0de437
erikwehrmann <erik.wehrmann@gmail.com>
2022-11-14 19:27:11 +0000

    Assert equal function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("cool", 'COOL');
+assertEqual(1 + 2, 3);
+assertEqual("Hello World!", "Hello World!");
