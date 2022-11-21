1180c2ec17661792f07197c9b433207b80af4f8d
erikwehrmann <erik.wehrmann@gmail.com>
2022-11-21 18:55:25 +0000

    Refactored eqArray Function

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(a, b,) {
   for (let i = 0; i < a.length; i++) {
     if (a[i] !== b[i]) {
@@ -15,15 +7,4 @@ const eqArrays = function(a, b,) {
   return true;
 }
 
-/*console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-*/
-
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+module.exports = eqArrays;
