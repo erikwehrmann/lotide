fa89797c542b89d09b2ef6a81e0ba3aa0d3c5b37
erikwehrmann <erik.wehrmann@gmail.com>
2022-11-21 22:22:01 +0000

    Refactored letterPosition function

--- letterPositions.js
+++ letterPositions.js
@@ -1,21 +1,3 @@
-const eqArrays = function(a, b,) {
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function(a, b) {
-  if (eqArrays(a, b) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${a} = ${b}`);
-  } else if (eqArrays(a, b) === false) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${a} = ${b}`);
-  }
-}
-
-
 const letterPositions = function(str) {
   const lowercaseStr = str.toLowerCase().split(" ").join("");
   const result = {};
@@ -30,4 +12,4 @@ const letterPositions = function(str) {
   return result;
 }
 
-assertArraysEqual(letterPositions('hello').e, [1]);
+module.exports = letterPositions;
