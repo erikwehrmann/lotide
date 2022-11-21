8d6c64d7a73e79df01fad679ffb09a260e6a56a6
erikwehrmann <erik.wehrmann@gmail.com>
2022-11-21 18:28:24 +0000

    Test files

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("cool", 'COOL');
+assertEqual(1 + 2, 3);
+assertEqual("Hello World!", "Hello World!");
