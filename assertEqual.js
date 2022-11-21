0047f0f2a46a3bc1694fd4923973905e7cb6b7f5
erikwehrmann <erik.wehrmann@gmail.com>
2022-11-21 18:29:33 +0000

    Refactored Assert Equals

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("cool", 'COOL');
-assertEqual(1 + 2, 3);
-assertEqual("Hello World!", "Hello World!");
+module.exports = assertEqual;
