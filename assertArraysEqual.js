1efec5e24c78bfe49932394526dc124aead8301d
erikwehrmann <erik.wehrmann@gmail.com>
2022-11-21 18:58:47 +0000

    Refactored assertArraysEqual Function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,4 @@
-const eqArrays = function(a, b,) {
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(a, b) {
   if (eqArrays(a, b) === true) {
